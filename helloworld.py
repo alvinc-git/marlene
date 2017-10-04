30184332427a7afa8bab01b59e7d230d81c0e758
Alvin Cura <alvin_cura@yahoo.com>
2017-10-04 10:39:21 -0700

    little file - alvinc

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# $Id$
+
+# first line, print a silly string
+print( 'Hello, World! My name is Maria.' )
+
+print( '\n\n' )			# a couple of spaces to make it readable
+
+# now show the number thingie-bob
+num = 36			# set the variable 'num' value to 36
+print( num )			# and print the variable 'num'
+
+quit()
